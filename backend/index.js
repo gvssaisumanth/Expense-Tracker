746217e8093be64c2493ef30b428c3e2dca40520
gvssaisumanth <gvssaisumanth24@gmail.com>
2024-07-07 16:27:21 -0400

    Using Express as Middle Ware

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,41 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+
 import { meregedResolvers } from "./resolvers/index.js";
 import { mergedTypeDefs } from "./typeDefs/index.js";
+
+const app = express();
+// Our httpServer handles incoming requests to our Express app.
+// Below, we tell Apollo Server to "drain" this httpServer,
+// enabling our servers to shut down gracefully.
+const httpServer = http.createServer(app);
 
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
   resolvers: meregedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-const { url } = await startStandaloneServer(server);
+// Ensure we wait for our server to start
+await server.start();
 
-console.log(`Server is up and Running at ${url}`);
+app.use(
+  "/",
+  cors(),
+  express.json(),
+  // expressMiddleware accepts the same arguments:
+  // an Apollo Server instance and optional configuration options
+  expressMiddleware(server, {
+    context: async ({ req }) => req,
+  })
+);
+
+// Modified server startup
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+console.log(`🚀 Server ready at http://localhost:4000/`);
